630c6e648d2e510bccacd2f6aad2ebca57c96af7
Reeba12 <reebasiddiqui456@gmail.com>
2022-09-02 23:49:09 -0700

    completed

---
+++ app.js
@@ -0,0 +1,276 @@
+// chapter alert
+// alert("ERROR! Please enter a valid pasword.")
+
+// alert("Welcome to Js land.....\nHappy Coding!");
+
+// alert("Welcome to Js land....");
+// alert("Happy Coding! \n")
+
+
+// chapter variable
+// var myname = "afra siddiqui";
+// document.write(myname);
+
+//  var message = "hello world";
+//  alert(message);
+
+// var name = "afra siddiqui";
+// alert(name);
+
+// var age ="19 years old";
+// alert(age);
+
+// var designation ="certified mobile app development";
+// alert(designation);
+
+// var name = "pizza \n pizz \n piz \n pi \n p" ;
+// alert(name);
+
+// var email = "My email is afrasiddiqui1333@.com" ;
+// alert(email);
+
+// var book = "A smarter way to learn JavaScript."
+// alert(book);
+
+// var write = "Yeh! I can write HTML content through javascript";
+// document.write(write);
+
+// var design ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(design);
+
+
+// CHAPTER : VARIABLES FOR NUMBER
+// var age = "I am 19 years old";
+// alert(age);
+
+// var birthYear = "MY birth year is 2002";
+// document.write(birthYear);
+
+// var shop = " “Afra Siddiqui ordered 5 T-shirt(s) on imtiaz shopping centr”. ";
+// document.write(shop); 
+
+
+
+// CHAPTER : LEGAL AND ILLEGAL
+// var legal ="b) Variable names can only contain ______, ______,______ and ______.\nFor example $my_1stVariable\nc) Variables must begin with a ______, ______ or\n_____. For example $name, _name or name\nd) Variable names are case _________\ne) Variable names should not be JS _________"
+// document.write(legal);
+
+//chapter maths expression
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1 + num2;
+// document.write( " Sum of " + num1 + " and " + num2 + " is " + sum) ;
+
+// var num1 = 2;
+// var num2 =5;
+// var minus = num1 - num2;
+// document.write(" substraction of " + num1 +" and " + num2 + " is " + minus );
+
+// var num1 = 2;
+// var num2 =4;
+// var minus = num1 / num2;
+// document.write(" division of " + num1 +" and " + num2 + " is " + minus );
+
+// var num1 = 2;
+// var num2 =5;
+// var minus = num1 * num2;
+// document.write(" substraction of " + num1 +" and " + num2 + " is " + minus );
+
+// var num1 ="undefined";
+// document.write("Value after variable declaration is "+ num1);
+// var num = 5;
+// document.write( "<br/> Initial number is " + num);
+// document.write( "<br/> value after increment is : " + num++);
+// document.write( "<br/> value after decrement is: " + num--);
+// document.write( "<br/> The remainder is: " + num/3);
+
+// var cost = 600  ;
+// document.write("Cost of one movie ticket is: " + cost + " KPR ");
+// document.write("<br/>Cost of 5 movie ticket is: " + cost*5);
+
+// var num = prompt("Enter the integer: ") ;
+// var i=0;
+// for( let i=1 ; i<=10 ; i++){
+//     document.write("<br/>" + num  + " X "+ i  + "= "+ num*i);
+    
+// }
+
+// var num = prompt("Enter the value:");
+// document.write(num+"C" + "=" + (num -32)*5/9 + "C");
+// document.write("<br/>"+num + "F" + "=" + (num *9/5)+32 + "F");
+
+// document.write("Price of item1 is 650", "<br/>Quality of item1 is 3");
+// document.write("Price of item2 is 100", "<br/>Quality of item2 is 7");
+// document.write("<br/>Shopping charges is 100");
+// var item1 = 650*3;
+// var item2 = 100*7;
+// var shoppingcharges = 100 ;
+// document.write("<br/> Total cost of your cart is :" + item1+item2+shoppingcharges);
+
+// document.write("Total marks = 980")
+// document.write("<br/>Marks obtained = 804")
+// var num1 = 980;
+// var num2 = 804;
+// document.write("<br/>Percentage =" + num2*100/num1);
+
+//chaper 6 to 9
+// var a = 10;
+// document.write("Result","<br/>The value of a is: "+a);
+// document.write("<br/>The value of ++a is: "+ ++a );
+// document.write("<br/> Now the value of a is:" + a);
+// document.write("<br/> The value of --a is: "+ --a );
+// document.write("<br/> Now the value of a is: " + a);
+
+
+// var a =2 ,b=1;
+// var result =--a - --b + ++b + b--;
+// document.write("<br/> Result:" + result);
+
+// var name = prompt("Enter your name")
+// document.write("WELCOME TO YOUR SHOP");
+
+// document.write("<table ><tr><td>SUBJECT  <td/><td>TOTAL MARKS  <td/><td>OBTAINED MARKS  <td/><td> PERCENTAGE<td/><tr/><table/>");
+// document.write("<table ><tr><td>ENGLISH  <td/><td>100  <td/><td>54  <td/><td> 54%<td/><tr/><table/>");
+// document.write("<table ><tr><td>MATHE  <td/><td>100  <td/><td>54 <td/><td>54%<td/><tr/><table/>");
+// document.write("<table ><tr><td>CHEMISTRY  <td/><td>48  <td/><td>48% <td/><td> <td/><tr/><table/>");
+
+//CHAPTER : 9 TO 11
+// var name = prompt("Enter the city");
+// if("name === karachi"){
+//     document.write("WELCOME TO CITY OF LIGHT");
+// }
+
+// var name = prompt("Enter the gender");
+// if(name == "male"){
+//     document.write("GOOD MORNING SIR");
+// }else{
+//     document.write("GOOD MORNING MAAM");
+// }
+
+// var signal = prompt("Enter yhe color:");
+// if(signal == "red"){
+//     document.write("YOU MUST STOP")
+// }else if ( signal == "yellow"){
+//     document.write("READY TO MOVE")
+// }else{
+//     document.write("MOVE NOW")
+// }
+
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");}
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+// var a = prompt("Enter the number");
+// if(a%3 == 0){
+// document.write("it is divisible by three")
+// }else{
+//     document.write("it is not divicible")
+// }
+
+//CHAPTER 12 AND 13
+// var ch = prompt("Enter the character:");
+// if (ch >= 'A' && ch <= 'Z'){
+//         document.write(" is an UpperCase character\n");
+ 
+// }else if (ch >= 'a' && ch <= 'z'){
+//     document.write(" is an LowerCase character\n");}
+//     else{
+//     document.write( " is not an aplhabetic character\n");}
+
+// var a = prompt("Enter the number:");
+// var b = prompt("Enter the number:");
+// if(a>b ){
+//     document.write(a +" is the largest");
+// }else if(b>a){
+//     document.write(b+" is the largest");
+// }
+
+// var a = prompt("Enter the number:");
+// if (a>0){
+//     document.write("Number is positive");
+// }if(a<0){
+//     document.write("Number is negative");
+// }else{
+//     document.write("Number is zero");
+// }
+    
+// var passward = prompt("Enter the passward:");
+// if (passward=="afra"){
+//     document.write("“Correct! The password you entered matches the original password”.")
+// }else{
+//     document.write("“Incorrect password”");
+// }
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// document.write("Good day");}
+// else{
+//     document.write( "Good evening");
+// }
+
+//chapter 14 to 16
+// var array =[];
+// array[0]= "afra";
+// array[1]= "sara";
+// array[2]= "reeba";
+// array[3]= "zunaire";
+// document.write(array);
+
+// var array =[1,2,3,4,5]
+// document.write(array);
+
+// var array =[1,"hey",3,4,"hello"];
+// document.write(array + "<br>");
+
+// var array = [ "SSC", "HSC", "BCS" ,"BS", "BCOM", "MS", "M.Phil", "PhD"];
+// document.write("<ul>")
+// for( var a=0;a<=7;a++){
+//     document.write("<li>"+ array[a] + "<br>" + "<li/>")
+// }
+// document.write("<ul/>")
+
+// var ary = [
+//     ["hira",34,500,"53%"],
+//     ["fatima",32,54,"59%" ],
+//     ["zoha",45,46,"23%"]
+// ];
+// for(var a=0 ; a<=3 ; a++){
+// document.write(ary[a]+ "<br>");}
+
+
+
